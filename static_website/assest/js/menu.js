065b0f61ceab8ba83c1b442f725e793d7ff225c9
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-07-22 18:24:40 +0300

    Update menu.js

--- static_website/assest/js/menu.js
+++ static_website/assest/js/menu.js
@@ -1,5 +1,41 @@
-console.log(window.location.pathname);
 const activeMenuItem = document.getElementById(window.location.pathname.replace("/", "").split(".")[0]);
 activeMenuItem.style.color = "#FF001F";
-
 //sayfanın hangi bölümünde bulunuyorsak(anasayfa,hakkımızda,iletişim,) ilgili linke renk verdik
+
+const activeMobileMenuItem = document.getElementById(`m-${window.location.pathname.replace("/", "").split(".")[0]}`);
+activeMobileMenuItem.style.color = "#FF001F";
+
+const foonterContainer = document.getElementById("footer-container");
+foonterContainer.style.position = "fixed";
+
+const sideMenu = document.getElementById("side-menu");
+
+var menuState = false;
+function toggleMenu() {
+    menuState = !menuState;
+    if (menuState) {
+        sideMenu.classList.add("isActive");
+    } else {
+        sideMenu.classList.remove("isActive");
+    }
+}
+
+function goTo(path) {
+    if (menuState) {
+        toggleMenu();
+        setTimeout(() => {
+            window.location.href = path;
+        }, 1000);
+    } else {
+        window.location.href = path;
+    }
+}
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        foonterContainer.style.position = "relative";
+    } else {
+        foonterContainer.style.position = "fixed";
+    }
+});
+
